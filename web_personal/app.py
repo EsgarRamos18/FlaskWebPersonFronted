b59d0460b9620cafc06540475a64b9d3667bab62
Esgar Ramos <eramoscarrascot@gmail.com>
2022-11-30 10:34:22 -0600

    2.4 RegisterFrom part 1

--- web_personal/app.py
+++ web_personal/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField
-from wtforms.validators import  DataRequired, Length, Email, EqualTo
+from wtforms import (StringField, PasswordField, SubmitField,
+EmailField, IntegerField, RadioField, SelectField, TextAreaField)
+from wtforms.validators import  DataRequired, Email
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] ='secret'
@@ -48,6 +49,15 @@ class LoginFrom(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Ingresar')
 
+class RegisterForm(FlaskForm):
+    name = StringField("Nombre")
+    last_name = StringField("Apellidos")
+    email = EmailField('Correo')
+    password = PasswordField('Contraseña')
+    phone = IntegerField('Telefono')
+    is_married = RadioField('Estado Civil', choices=[('True', 'Casado'), ('False', 'Soltero')])
+    gender = SelectField('Gender', choices=[('male', 'Masculino'), ('female', 'Femenino'), ('other', 'Otro')])
+    submit = SubmitField('Registrar')
 ################################
 #           Routes Login
 ################################
@@ -65,19 +75,13 @@ def login():
 
 @app.route('/auth/register')
 def register():
-    return render_template('auth/register.html')
-
-@app.route('/welcome', methods=['GET','POST'])
-def welcome(form):
-    if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
-        return render_template('admin/index.html', email=email)
-    
-    return redirect(url_for['login'])
+    form = RegisterForm()
+    return render_template('auth/register.html', form=form)
 
 @app.errorhandler(404)
 def page_error_not_found(e):
     return render_template('error/404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
