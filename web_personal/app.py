e293a4ba28b5e79dab5e30b0fa743b4e932f4776
Esgar Ramos <eramoscarrascot@gmail.com>
2022-12-02 22:25:56 -0600

    3.3 Blueprint Error 404

--- web_personal/app.py
+++ web_personal/app.py
@@ -1,13 +1,11 @@
-from flask import Flask, redirect, render_template, request, url_for
-from flask_wtf import FlaskForm
-from wtforms import (StringField, PasswordField, SubmitField,
-EmailField, IntegerField, RadioField, SelectField, TextAreaField)
-from wtforms.validators import  DataRequired, Email
+from flask import Flask
+
 
 
 ########## Nuestros modulos ###########
 from home.views import home_blueprint
 from auth.views import auth_blueprint
+from error_pages.handlers import error_pages_blueprint
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] ='secret'
@@ -15,15 +13,14 @@ app.config['SECRET_KEY'] ='secret'
 
 
 
-@app.errorhandler(404)
-def page_error_not_found(e):
-    return render_template('error/404.html'), 404
+
 
 
 ################### app ##################
 
 app.register_blueprint(home_blueprint, url_prefix='/')
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
+app.register_blueprint(error_pages_blueprint, url_prefix='/error')
 
 if __name__ == '__main__':
     app.run(debug=True)
